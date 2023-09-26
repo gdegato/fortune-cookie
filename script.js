0c42045cb621202c3fce2a19eb0454f86c81d2da
gdegato <rosele.tst@gmail.com>
2023-09-26 18:58:37 -0300

    novas frases

--- script.js
+++ script.js
@@ -14,7 +14,17 @@ let textArray = [
   'Nunca subestime sua incapacidade.',
   'O problema não está na segunda-feira, está na sua vida.',
   'Pode ser que o seu propósito na vida seja servir de aviso para os outros.',
-  'Não sabendo que era impossível, foi lá e soube.',
+    'Não sabendo que era impossível, foi lá e soube.',
+    'Hoje é o primeiro dia do resto da sua vida. Mas ontem também foi, e veja como acabou.',
+    'Daqui pra frente, só pra trás',
+    'As piores coisas da sua vida provavelmente ainda não aconteceram com você.',
+    'Aqueles que duvidam de sua capacidade provavelmente têm um motivo válido.',
+    'A única coisa que todas as suas falhas têm em comum é você.',
+    'A recompensa pelo bom trabalho é mais trabalho.',
+    'O sonho de todo profissional é abandonar sua profissão'
+
+
+  
 ]
 
 let randomIndex = Math.floor(Math.random() * textArray.length)
